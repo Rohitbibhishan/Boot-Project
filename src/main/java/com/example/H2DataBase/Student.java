6f2e549f2c96fc1ae3b4d2ada43d2d8d3e09281b
Elite <rohit.raj.mehta723@gmailcom>
2024-05-03 15:46:08 +0530

    second commit

--- src/main/java/com/example/H2DataBase/Student.java
+++ src/main/java/com/example/H2DataBase/Student.java
@@ -12,7 +12,7 @@ public class Student {
     private Long id;
     private String name;
     private int age;
-    private int rollno=123;
+    private int rollno=12;
     private String email;
     //rollno= 123;
 
